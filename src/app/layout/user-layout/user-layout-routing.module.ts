22e49affaea0ab3d82f659c3b4ca4edd24f3dd58
namatitiben <namatitiben@gmail.com>
2019-09-28 22:03:16 +0300

    set up blog listing page

--- src/app/layout/user-layout/user-layout-routing.module.ts
+++ src/app/layout/user-layout/user-layout-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { BlogListComponent } from 'src/app/blog/blog-list/blog-list.component';
+import { UserMainComponent } from './user-main/user-main.component';
 
 
 const routes: Routes = [
-  {path: '', component: BlogListComponent}
+  {path: '', component: UserMainComponent, children: [ {path: '', component: BlogListComponent}]}
 ];
 
 @NgModule({
