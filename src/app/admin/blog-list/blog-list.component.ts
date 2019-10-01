2358fa980dfdf4e5f16bb336b89f7fb0b1cb3a94
namatitiben <namatitiben@gmail.com>
2019-10-01 05:05:05 +0300

    fix issues with delete and update

--- src/app/admin/blog-list/blog-list.component.ts
+++ src/app/admin/blog-list/blog-list.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit, TemplateRef } from '@angular/core';
 
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
-import { tap } from 'rxjs/operators';
+import { tap, catchError } from 'rxjs/operators';
 
 import { BlogService } from 'src/app/services/blog.service';
 import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-blog-list',
@@ -75,20 +76,33 @@ export class BlogListComponent implements OnInit {
   postBlog() {
     if (this.postForm.valid) {
       if (this.selectedBlog) {
-        // update
+        // update blog
+
+        // deep copy the form value, esp for new records that user adds
+        const updatedValue = JSON.parse(JSON.stringify(this.postForm.value));
+
+        const index = this.blogService.blogs.findIndex(
+          item => item.id === this.selectedBlog.id
+        );
         const sub = this.blogService
           .updateBlog(this.postForm.value)
           .pipe(
             tap(blog => {
               // add new blog to the list
-              const index = this.blogService.blogs.findIndex(
-                item => item.id === blog.id
-              );
+
               this.blogService.blogs[index] = blog;
 
               if (sub) {
                 sub.unsubscribe();
               }
+            }),
+
+            catchError(err => {
+              // you see, the api is fake and our changes are not persisted
+              // this just takes care of that
+              this.blogService.blogs[index] = updatedValue;
+
+              return of(err);
             })
           )
           .subscribe();
@@ -121,10 +135,20 @@ export class BlogListComponent implements OnInit {
     const sub = this.blogService
       .deletePost(this.selectedBlog.id)
       .pipe(
-        tap(data => {
-          console.log(data);
+        tap(_ => {
+          // remove blog from the list
+          const index = this.blogService.blogs.findIndex(
+            item => item.id === this.selectedBlog.id
+          );
+          if (index >= 0) {
+            this.blogService.blogs.splice(index, 1);
+          }
+
+          // close the modal
           this.modalRef.hide();
           this.selectedBlog = null;
+
+          // unsubscribe
           if (sub) {
             sub.unsubscribe();
           }
